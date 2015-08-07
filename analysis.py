eb15ec91a6f2f89facc90e951dd657ae70e161c5
Rory Scott <rory09@gmail.com>
2015-08-07 11:52:56 +0100

    Tidying comments

--- analysis.py
+++ analysis.py
@@ -2,9 +2,9 @@ import xml.etree.ElementTree as ET
 import requests
 import sys
 
-# Canada, WFP, Unicef
-
-'''README: '''
+'''README: to use this script, you must include 2 or more arguments when calling it. The first must be the IATI identifier of the Organisation
+you wish to examine, and then one or more country codes afterwards. The csv output is one budget per row, with the budget amound multiplied
+by the percentage assigned to the country of interest within the given activity.'''
 
 org = sys.argv[1]
 start = '2015-01-01'
@@ -15,13 +15,15 @@ print("orgID, country, budget-start, budget-end, value, currency, valuation_date
 # Repeat this process for every country given
 for i in range(2,len(sys.argv)):
 
+	# The following lines translate the user provided arguments into an html api call for the IATI registry, download an xml response,
+	# and then parse it into memory
 	country = sys.argv[i]
-
 	payload = {'reporting-org': org, 'recipient-country': country, 'end-date__gt': start, 'stream':'True'}
 	response = requests.get("http://datastore.iatistandard.org/api/1/access/activity.xml", params=payload)
 	result = ET.fromstring(response.content)
 	activities = result.findall("./iati-activities/iati-activity")
 
+	# Set our initial values
 	number_of_activities = 0
 	activities_without_budgets = 0
 	total_committed = 0
@@ -33,10 +35,10 @@ for i in range(2,len(sys.argv)):
 		# Then for each of those activies, output all individual budget objects along with there values, value dates, and ranges.
 		for activity in activities:
 			
-			#counter for the budget collisions found in this activity
+			# counter for the budget collisions found in this activity
 			current_budget_collisions = 0
 
-			#count total number of activities
+			# count total number of activities
 			number_of_activities += 1
 			
 			# percentage of this activity that applies to the country of interest
@@ -47,20 +49,16 @@ for i in range(2,len(sys.argv)):
 					if countries.attrib['code'] == country:
 						country_weighting = (countries.attrib['percentage'])
 
-			#Extract budget elements and print how many there are
+			# Extract budget elements
 			budget_elements = activity.findall("budget")
 			
-			#Are there budget elements?
+			# If there are any budget elements
 			if len(budget_elements) > 0:
-				# Create a budget dictionary so that we can compare revised / non-revised budgets and insert the approapriate one
-				non_colliding_budgets = {}
-				# For each budget element
+				# then iterate through each budget element
 				for budget in budget_elements:
-					# Store whether it's revised or original
-					b_type = budget.attrib['type']
-					# For each detail of the budget
+					# then iterate through each detail of the given budget
 					for child in budget:
-						# if the detail is not the value element
+						# and record the values we're interested in
 						if child.tag == 'period-start':
 							budget_start = child.attrib['iso-date']
 						elif child.tag == 'period-end':
@@ -68,13 +66,8 @@ for i in range(2,len(sys.argv)):
 						elif child.tag == 'value':
 							valuation_date = child.attrib['value-date']
 							budget_value = float(child.text)/100.0* float(country_weighting)
-					#Output the budgets
+					# Output the relevent information for each budget
 					print "%s,%s,%s,%s,%f,%s" % (org, country, budget_start, budget_end, budget_value, valuation_date)
-
-
-			else:
-				activities_without_budgets += 1
-						
 
 	# If there are no activities, output and error message
 	else: print "%s has no activities for %s!" % (org, country)
--- initial_coverage_review.py
+++ initial_coverage_review.py
@@ -5,7 +5,7 @@ import sys
 # Canada, WFP, Unicef
 
 '''README: to use this script, you must include 2 or more arguments when calling it. The first must be the IATI identifier of the Organisation
-you wish to examine, and then one or more country codes afterwards. For each counry code provided, a row will be appended into the csv terminal output.'''
+you wish to examine, and then one or more country codes afterwards. For each country code provided, a row will be appended into the csv terminal output.'''
 
 org = sys.argv[1]
 start = '2015-01-01'
@@ -16,37 +16,34 @@ print("org, country, start, number_of_activities, activities_without_budgets, pe
 # Repeat this process for every country given
 for i in range(2,len(sys.argv)):
 
+	# The following lines translate the user provided arguments into an html api call for the IATI registry, download an xml response,
+	# and then parse it into memory
 	country = sys.argv[i]
-
 	payload = {'reporting-org': org, 'recipient-country': country, 'end-date__gt': start, 'stream':'True'}
 	response = requests.get("http://datastore.iatistandard.org/api/1/access/activity.xml", params=payload)
 	result = ET.fromstring(response.content)
 	activities = result.findall("./iati-activities/iati-activity")
 
+	# Set our initial values
 	number_of_activities = 0
 	activities_without_budgets = 0
 	total_committed = 0
 	total_budgets = 0
 	budget_collisions = 0
 
-
-	#start_date = None <---Input date (YYYY-MM-DD)
-	#end_date = None <-----Input date (YYYY-MM-DD)
-
-
-	#For each activity
+	# For each activity
 	for activity in activities:
 		
-		#counter for the budget collisions found in this activity
+		# counter for the budget collisions found in this activity
 		current_budget_collisions = 0
 
-		#count total number of activities
+		# count total number of activities
 		number_of_activities += 1
 
-		#Extract budget elements and print how many there are
+		# Extract budget elements and print how many there are
 		budget_elements = activity.findall("budget")
 		
-		#Are there budget elements?
+		# Are there budget elements?
 		if len(budget_elements) > 0:
 			
 			# Store the number of budgets
@@ -65,12 +62,12 @@ for i in range(2,len(sys.argv)):
 							found_budgets[date_string] = 1 
 						else:	current_budget_collisions += 1
 
-			#add the budget collisions to the total
+			# add the budget collisions to the total
 			budget_collisions += current_budget_collisions
 		
 		else:
 			activities_without_budgets += 1	
 					
-	#Output the total, activities_without_budgets
+	# Output the total, activities_without_budgets
 	percent = float(number_of_activities - activities_without_budgets) / float(number_of_activities) * 100
 	print "%s,%s,%s,%i,%i,%f,%i,%i,%s" % (org, country, start, number_of_activities, activities_without_budgets, percent, total_budgets, budget_collisions, response.url)
